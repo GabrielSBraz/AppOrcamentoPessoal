8929ffed9011e564326412bddb8ffde920707178
Gabriel de Sousa Braz <gabrielbraz014@outlook.com>
2021-01-22 01:24:47 -0300

    Cadastro de despesa
    implementação da função cadastrarDespesa(), utilizando o id do css para recuperar seu valor e armazenar numa variável do tipo let.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert('OK')
+function cadastrarDespesa() {
+
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+	console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+
+}
